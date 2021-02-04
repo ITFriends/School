738b96c4c33774b4f1c89073b6ae765eaa888945
Vadimirych <VadimirychDnepr@gmail.com>
2021-02-03 22:51:43 -0500

    C# - Beginner, Lesson 3,  Sample

---
+++ C#-Beginner/Lesson 3/Car.Properties/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Car.Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var car = new Car();
+            car.Milleage = int.Parse(Console.ReadLine());
+            Console.WriteLine(car.Milleage);
+            Console.WriteLine("Let's go N kilometers:");
+            var distance = int.Parse(Console.ReadLine());
+            car.Go(distance);
+            Console.WriteLine(car.Milleage);
+        }
+    }
+
+    class Car
+    {
+        private int milleage;
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Milleage
+        {
+            get { return milleage; }
+            set { milleage = value; }
+        }
+
+        public string Rank
+        {
+            get
+            {
+                if (milleage < 100000)
+                {
+                    return "A";
+                }
+                else
+                {
+                    return "B";
+                }
+            }
+        }
+
+        public void Go(int distance)
+        {
+            Milleage = Milleage + distance;
+        }
+    }
+}
