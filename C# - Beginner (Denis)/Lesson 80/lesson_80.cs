e17d24644b5951154c5e5e611af7ef4bd55696b8
DenisPavlenko <denispavlenko7610new@gmail.com>
2021-02-22 18:25:51 +0200

    add 80 lesson

---
+++ C# - Beginner (Denis)/Lesson 80/lesson_80.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            reader.ReadEbook();
+            reader.ReadBook(); //Library
+        }
+    }
+
+    class Reader
+    {
+        Lazy<Library> library = new Lazy<Library>();
+        public void ReadBook()
+        {
+            library.Value.GetBook();
+            Console.WriteLine("Читаем бумажную книгу");
+        }
+
+        public void ReadEbook()
+        {
+            Console.WriteLine("Читаем книгу на компьютере");
+        }
+    }
+
+    class Library
+    {
+        private string[] books = new string[99];
+
+        public void GetBook()
+        {
+            Console.WriteLine("Выдаем книгу читателю");
+        }
+    }
+}
